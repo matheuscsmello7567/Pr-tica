55a9f9259d0f62fd8815126e2a6a5e2dee76684a
matheuscsmello7567 <matheuscsmello@hotmail.com>
2024-10-22 13:24:27 -0300

    Commit classes agencia e companhiaAerea

---
+++ Aeroporto/agencia.java
@@ -0,0 +1,14 @@
+public class agencia {
+
+    String nome_agencia;
+    String cnpj;
+    
+    public String getNome(){
+    return nome_agencia;
+    }
+
+    public String getCnpj(){
+        return cnpj;
+    }
+
+}
---
+++ Aeroporto/companhiaAerea.java
@@ -0,0 +1,24 @@
+public class companhiaAerea{
+
+    String nome_companhia;
+    int codigo;
+    String razaoSocial;
+    String cnpj;
+
+    public String getNome_companhia(){
+        return nome_companhia;
+    }
+
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public String getRazaoSocial(){
+        return razaoSocial;
+    }
+
+    public String getCnpj(){
+        return cnpj;
+    }
+
+}
